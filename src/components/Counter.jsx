e96f2037bad93e4a5e76652c9289f30ddfb2eff9
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2024-02-23 22:44:45 +0600

    Create Redux State Slice & add Reducers to use Redux State and Actions

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,23 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, incrementByValue } from '../redux/features/counter/counterSlice'
+
+
 
 const Counter = () => {
+    const {count} = useSelector((state) => state.counter)
+    const dispatch = useDispatch()
+
     return (
         <div>
             
+            <button onClick={() => dispatch(incrementByValue(5))}>Increment by 5</button>
+            <br />
+            <button onClick={() => dispatch(increment())}>Increment</button>
+            <div>
+                <h1>{count}</h1>
+            </div>
+            <button
+            onClick={() => dispatch(decrement())}>Decrement</button>
         </div>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Counter from "../components/Counter";
 
 
 const Home = () => {
     return (
         <div>
-            
+            Home
+            <Counter/>
         </div>
     );
 };
