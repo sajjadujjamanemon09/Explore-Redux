11a1bb5544840366b5f8b9a5c56e7b6ce1a8bad2
Sajjaduj Jaman Emon <mdsajjadujjaman09@gmail.com>
2024-02-24 17:56:07 +0600

    Implement middleware

--- src/redux/Store.jsx
+++ src/redux/Store.jsx
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from'./features/counter/counterSlice'
 
+
 export const store = configureStore({
     reducer: {
       counter: counterReducer,
     },
+    middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat()
   })
 
   export default store;
